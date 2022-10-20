2815acfcca34299fdda461934c3bc95cb3e0897a
richard98 <274912848@qq.com>
2022-10-20 22:13:21 +0800

    A新增提交

--- src/main/java/ClassA.java
+++ src/main/java/ClassA.java
@@ -1,9 +1,11 @@
 /**
  * @author Admin
- * @date 2022/10/20
  */
 public class ClassA {
     void methodA() {
         System.out.println("method A is running");
+    }
+    void newMethodA() {
+        System.out.println("new method A is running");
     }
 }
